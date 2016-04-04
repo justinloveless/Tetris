d3cda58c14ee3871ff367fa0344873c264ff1e3c
Justin Loveless <justinlovelessx@gmail.com>
2016-04-03 21:04:20 -0500

    test commit

--- Tetris/src/EventController.java
+++ Tetris/src/EventController.java
@@ -14,7 +14,6 @@ import java.awt.event.KeyEvent;
 import javax.swing.JFrame;
 import javax.swing.Timer;
 
-/// This is a useless comment to test out committing to GitHub
 
 public class EventController extends KeyAdapter implements ActionListener {
 
